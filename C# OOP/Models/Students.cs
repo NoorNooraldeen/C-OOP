a275ca53aa70f80ab08f1ea0acc8bc1dbea19ce2
NoorN <NoorN@NOOR>
2024-12-24 20:50:07 +0300

    Add project files.

---
+++ C# OOP/Models/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__OOP.Models
+
+
+{
+    public class Students
+    {
+
+        public string Name;
+        public string Major;
+        public double GPA;
+
+        public string studentSummary(string name, string major, double GPA)
+        {
+
+            return "student name is:  " + name + "\nstudent major is: " + major + "\nstudent gpa is: " + GPA;
+        }
+    }
+}
+
---
+++ C# OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using C__OOP.Models;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+Students studentclass = new Students();
+string name, major;
+double Gpa;
+Console.WriteLine("enter your name: ");
+name = Console.ReadLine();
+Console.WriteLine("enter your major: ");
+major = Console.ReadLine();
+Console.WriteLine("enter your GPA: ");
+Gpa = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(studentclass.studentSummary(name, major, Gpa));
+
+
+
+
+
